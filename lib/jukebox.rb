38acb5fcd433e7c8f1869bdf14a9513dfee6a5d1
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-12 01:57:44 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -33,6 +33,7 @@ def play(songs)
   def run 
     puts "Please enter a command:"
     command = gets.strip
+    while command !exit
     if command == "help"
       help
       elsif command == "list"
