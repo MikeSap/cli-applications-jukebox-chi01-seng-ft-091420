1bc491c11d0b055f490f41cb47d437a256756661
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-12 02:01:29 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -41,9 +41,9 @@ def play(songs)
     if command == "help"
       help
       elsif command == "list"
-      list
+      list(songs)
       elsif command == "play"
-      play
+      play(songs)
     else
       puts "Invalid input, please try again"
     end
